b76d04836ebb1b3a4a6e4f240bc163fc490dc5a9
Renato Salvador <renato.salvador@bolttech.io>
2022-05-21 01:02:06 +0100

    add jest config paths core

--- src/@core/jest.config.ts
+++ src/@core/jest.config.ts
@@ -100,7 +100,11 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '#category/(.*)$': '<rootDir>/category/$1',
+    '#shared/(.*)$': '<rootDir>/shared/$1',
+  },
+
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
