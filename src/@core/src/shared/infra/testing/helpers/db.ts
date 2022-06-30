a46bc3f6a3abcc3acdb94c4456a5d4b401ca8a5d
Renato Alves <renatoalves@Renatos-MacBook-Pro.local>
2022-06-30 17:49:23 +0100

    update sequelize host

--- src/@core/src/shared/infra/testing/helpers/db.ts
+++ src/@core/src/shared/infra/testing/helpers/db.ts
@@ -1,33 +1,33 @@
-import { Sequelize, SequelizeOptions } from "sequelize-typescript";
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
 
 const sequelizeOptions: SequelizeOptions = {
-  dialect: "sqlite",
-  host: ":memory",
-  logging: false,
+    dialect: 'sqlite',
+    host: ':memory:',
+    logging: false,
 };
 
 export function setupSequelize(options: SequelizeOptions = {}) {
-  let _sequelize: Sequelize;
+    let _sequelize: Sequelize;
 
-  beforeAll(
-    () =>
-      (_sequelize = new Sequelize({
-        ...sequelizeOptions,
-        ...options,
-      }))
-  );
+    beforeAll(
+        () =>
+            (_sequelize = new Sequelize({
+                ...sequelizeOptions,
+                ...options,
+            })),
+    );
 
-  beforeEach(async () => {
-    await _sequelize.sync({ force: true });
-  });
+    beforeEach(async () => {
+        await _sequelize.sync({ force: true });
+    });
 
-  afterAll(async () => {
-    await _sequelize.close();
-  });
+    afterAll(async () => {
+        await _sequelize.close();
+    });
 
-  return {
-    get sequelize() {
-      return _sequelize;
-    },
-  };
+    return {
+        get sequelize() {
+            return _sequelize;
+        },
+    };
 }
