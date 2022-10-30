ffbba53219bf7599ed8d39ca07f8bc67f26a97db
Renato Alves <renatoalves@Renatos-MBP.Home>
2022-10-30 10:46:10 +0000

    config e2e

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -4,7 +4,6 @@ export default {
     color: 'magentaBright',
   },
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.*\\..*spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': '@swc/jest',
@@ -12,14 +11,13 @@ export default {
   collectCoverageFrom: ['**/*.(t|j)s'],
   testEnvironment: 'node',
   moduleNameMapper: {
-    'mycore/(.*)': '<rootDir>/../../../node_modules/mycore/dist/$1',
-    '#shared/(.*)$': '<rootDir>/../../../node_modules/mycore/dist/shared/$1',
-    '#category/(.*)$':
-      '<rootDir>/../../../node_modules/mycore/dist/category/$1',
+    'mycore/(.*)': '<rootDir>/../../node_modules/mycore/dist/$1',
+    '#shared/(.*)$': '<rootDir>/../../node_modules/mycore/dist/shared/$1',
+    '#category/(.*)$': '<rootDir>/../../node_modules/mycore/dist/category/$1',
   },
   setupFilesAfterEnv: [
-    '../../@core/src/shared/domain/tests/setup.ts',
-    '../../@core/src/shared/domain/tests/jest.ts',
+    '../@core/src/shared/domain/tests/setup.ts',
+    '../@core/src/shared/domain/tests/jest.ts',
   ],
   coverageDirectory: '../coverage',
   coverageProvider: 'v8',
